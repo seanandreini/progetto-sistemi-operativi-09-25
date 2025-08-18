712b419eaf9bf932baeb143e79ce7a6d4340309a
Sean <sean.andreini@gmail.com>
2025-08-18 17:59:56 +0200

    Refactor server.c to implement socket handling and connection acceptance logic Co-authored-by: Andrea Bertini

--- server.c
+++ server.c
@@ -1,18 +1,61 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#define SERVER_PORT 12345
+#define SERVER_ADDRESS "127.0.0.1"
 
 
 int main(int argc, char *argv[]){
   
-  int sockfd;
+  int socketfd, clientfd;
+  struct sockaddr_in serverAddress, clientAddress;
 
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  // creazione socket
+  if ((socketfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+    perror("Error creating socket");
+    exit(-1);
+  }
+  printf("Server socket created successfully.\n");
+
+  // definizio indirizzo server
+  serverAddress.sin_family = AF_INET;
+  serverAddress.sin_port = htons(SERVER_PORT); 
+  serverAddress.sin_addr.s_addr = SERVER_ADDRESS;
+
+  // binding socket
+  if(bind(socketfd, (struct sockaddr*) &serverAddress, sizeof(serverAddress)) == -1) {
+    perror("Error binding socket");
+    close(socketfd);
+    exit(-1);
+  }
+
+  // listen for incoming connections
+  if(listen(socketfd, 1) == -1) {
+    perror("Error listening on socket");
+    close(socketfd);
+    exit(-1);
+  }
+
+  printf("Server is listening on port %d\n", SERVER_PORT);
 
 
+  // accept incoming connections
+  while(1){
+    if((clientfd=accept(socketfd, (struct sockaddr*) &clientAddress, sizeof(clientAddress) == -1))) {
+      perror("Error accepting connection");
+      close(socketfd);
+    }
+    else {
+      printf("Connection accepted from %s:%d\n", inet_ntoa(clientAddress.sin_addr), ntohs(clientAddress.sin_port));
+      write(clientfd, "Hello from server!\n", 19);
 
-  
-  
+      close(clientfd);
+    }
+  }
   
   return 0;
 }
